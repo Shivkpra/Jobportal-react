f24f4c519cb1122c9b6b33ebae0ba3269ef3933d
bawa-yatin <Yatin.bawa@mindbowser.com>
2022-07-12 15:22:20 +0530

    Completed UI Design of Talent/Company Details Page

--- jobportalfrontend/src/App.js
+++ jobportalfrontend/src/App.js
@@ -4,8 +4,11 @@ import "./App.css";
 import HomeScreen from "./Components/Home Screen/Navbar";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
+import CompanyDetailsPage from "./Components/Detail Forms/CompanyDetailForm";
+import TalentDetailsPage from "./Components/Detail Forms/TalentDetailForm";
+
 function App() {
-  return <Signup/>;
+  return <TalentDetailsPage />;
 }
 
 export default App;
