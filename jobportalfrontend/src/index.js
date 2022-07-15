75a965216d3a4dccf1af003b2542f0610509ced5
Rohit Ghule <rohit.ghule@mindbowser.com>
2022-07-15 10:22:56 +0530

    added login logout urls,actions,state,reducers

--- jobportalfrontend/src/index.js
+++ jobportalfrontend/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from 'react-redux';
-import store from './Components/Services/store';
+import {store} from './Components/Services/store';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
